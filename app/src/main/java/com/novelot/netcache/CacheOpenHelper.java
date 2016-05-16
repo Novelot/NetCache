fd95af799c114eaeaddac08ee002cd2ef379d72b
liuyl <liuyl@kaolafm.com>
2016-05-16 13:14:01 +0800

    1.uri 唯一;

--- app/src/main/java/com/novelot/netcache/CacheOpenHelper.java
+++ app/src/main/java/com/novelot/netcache/CacheOpenHelper.java
@@ -3,6 +3,7 @@ package com.novelot.netcache;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by V on 2016/5/9.
@@ -12,12 +13,12 @@ public class CacheOpenHelper extends SQLiteOpenHelper {
     private static final int DB_VERSION = 1;
     public static final String TABLE_NAME = "t_cache";
 
-    public static class Columns {
-        public static final String URL = "url";
-        public static final String RESULT = "result";
-        public static final String ETAG = "etag";
-        public static final String LAST_MODIFIED = "last_modified";
-        public static final String UPDATE_TIME = "update_time";
+    public interface Columns extends BaseColumns {
+        String URL = "url";
+        String RESULT = "result";
+        String ETAG = "etag";
+        String LAST_MODIFIED = "last_modified";
+        String UPDATE_TIME = "update_time";
     }
 
     public CacheOpenHelper(Context context) {
@@ -28,12 +29,14 @@ public class CacheOpenHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         StringBuffer sBuffer = new StringBuffer();
         sBuffer.append("CREATE TABLE " + TABLE_NAME + " (");
-        sBuffer.append("_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+        sBuffer.append(Columns._ID).append(" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+        sBuffer.append(Columns._COUNT).append(" INTEGER,");
         sBuffer.append(Columns.URL).append(" TEXT UNIQUE,");
         sBuffer.append(Columns.RESULT).append(" TEXT,");
         sBuffer.append(Columns.ETAG).append(" TEXT,");
         sBuffer.append(Columns.LAST_MODIFIED).append(" LONG,");
         sBuffer.append(Columns.UPDATE_TIME).append(" LONG");
+
         sBuffer.append(");");
         db.execSQL(sBuffer.toString());
     }
